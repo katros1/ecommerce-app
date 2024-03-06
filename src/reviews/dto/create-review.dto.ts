edd78bfdfa6b5f3235663d2a9264fd6704af9797
katros1 <katros250@gmail.com>
2024-03-06 12:56:31 +0200

    add reviews

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,16 @@
-export class CreateReviewDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateReviewDto {
+
+    @IsNotEmpty({message:'Product should not be empty'})
+    @IsNumber({}, {message:'Product Id should be number'})
+    productId: number;
+
+    @IsNotEmpty({message:'rates should not be empty'})
+    @IsNumber({}, {message:'rates Id should be number'})
+    ratings: number;
+
+    @IsNotEmpty({message:'comment should not be empty'})
+    @IsString()
+    comment: string;
+}
--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,25 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { AuthorizeRoles } from 'src/utility/decorators/authorize-roles.decorator';
+import { Roles } from 'src/utility/common/user-roles.enum';
+import { AuthenticationGuard } from 'src/utility/guards/authentication.guard';
+import { AuthorizeGuard } from 'src/utility/guards/authorization.guard';
+import { CurrentUser } from 'src/utility/decorators/current-user.decorators';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
-
+ 
+  @AuthorizeRoles(Roles.ADMIN)
+  @UseGuards(AuthenticationGuard, AuthorizeGuard)
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  async create(@Body() createReviewDto: CreateReviewDto, @CurrentUser() currentUser: UserEntity ) {
+    return await this.reviewsService.create(createReviewDto, currentUser);
   }
 
+  // @Get()
+  // async findAll() {
+  //   return await this.reviewsService.findAll();
+  // }
+
   @Get()
-  findAll() {
-    return this.reviewsService.findAll();
+  async findAllByProd(@Body('productId') prodId: number) {
+    return await this.reviewsService.findAllByProduct(+prodId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.reviewsService.findOne(+id);
   }
 
   @Patch(':id')
@@ -27,6 +40,9 @@ export class ReviewsController {
     return this.reviewsService.update(+id, updateReviewDto);
   }
 
+
+  @AuthorizeRoles(Roles.ADMIN)
+  @UseGuards(AuthenticationGuard, AuthorizeGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.reviewsService.remove(+id);
